884d95f5210d96904f7c906fe9a5069a37be2f4c
Aodhan Walker <54781261+awebdragon@users.noreply.github.com>
2025-09-15 22:34:55 -0600

    Updates

---
+++ v2/fields.js
@@ -0,0 +1,20 @@
+export const myFields = [
+  "CFLD",
+  "RFLD",
+  "HRCN",
+  // "AVLN",
+  //  "CWAV",
+  //  "DRGT",
+  //  "ERQK",
+  //  "HAIL",
+  //  "HWAV",
+  //  "ISTM",
+  //  "LNDS",
+  //  "LTNG",
+  //  "SWND",
+  //  "TRND",
+  //  "TSUN",
+  //  "VLCN",
+  //  "WFIR",
+  //  "WNTW"
+];
